c20c09255683fc12575b75ea1d074a1ca3cb9da9
Kate Efimova <kate.efimova0@gmail.com>
2019-02-25 09:28:30 -0800

    Create mdn.js

---
+++ V.1-CodePen Preview/mdn.js
@@ -0,0 +1,29 @@
+let ele = document.getElementById('form');
+let icon = document.getElementById('search-icon');
+
+if (ele.addEventListener) {
+  ele.addEventListener("submit", callback, false);  //Modern browsers
+  icon.addEventListener("click", callback);
+} else if(ele.attachEvent) {
+  ele.attachEvent('onsubmit', callback);            //Old IE
+  icon.addEventListener("click", callback);
+}
+
+function callback(e) {
+  let arrayInput = document.getElementsByTagName("input")[0].value.split(" ");
+  
+  if (arrayInput[0] === undefined) {
+    alert("Bad input. Try again");
+    e.preventDefault(); 
+  }
+  
+  if (arrayInput[1] === undefined) {
+    let website = `https://developer.mozilla.org/en-US/search?q=${arrayInput[0]}`;
+    window.open(website,'_blank');
+  } else {
+    let website = 'https://developer.mozilla.org/en-US/search?q=';
+    let suffix = arrayInput.join('+');
+    
+    window.open(website + suffix,'_blank');
+  }
+}
