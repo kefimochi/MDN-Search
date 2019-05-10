f765d6009245d44f6a726725775004858ae0448c
Kate Efimova <kate.efimova0@gmail.com>
2019-05-10 08:19:56 -0700

    Add files via upload

---
+++ V.1.1-MVP/background.js
@@ -0,0 +1,5 @@
+chrome.runtime.onInstalled.addListener(function() {
+  // add an action here
+});
+
+// It will always be running while your extension is turned on and is useful for listening to different events, such as keyboard presses, or for navigating to different pages.
