51074ea6bf901f75ab401d82634b7f0f275336e1
Kate Efimova <kate.efimova0@gmail.com>
2019-05-20 00:13:21 -0700

    Refactoring styling

--- V.1.2-UX Friendly/content.js
+++ V.1.2-UX Friendly/content.js
@@ -3,7 +3,7 @@ let icon = document.getElementById("search-icon");
 
 // DOM Elements
 let searchBar = document.getElementById("search-bar");
-let searchListDOM = document.getElementById("serach-list");
+let searchListDOM = document.getElementById("search-list");
 
 // google custom search keys
 let API_KEY = "AIzaSyAF6PrEEbmZOb1lJqVWwiAQpg3ysI0N608";
@@ -28,7 +28,7 @@ function callback(e) {
   if (arrayInput[1] === undefined) {
     let website = `https://developer.mozilla.org/en-US/search?q=${
       arrayInput[0]
-      }`;
+    }`;
     window.open(website, "_blank");
   } else {
     let website = "https://developer.mozilla.org/en-US/search?q=";
@@ -48,30 +48,47 @@ searchBar.addEventListener("input", () => {
 });
 
 /**
- * Google Custom Search | 
+ * Google Custom Search |
  * Resource https://developers.google.com/custom-search/v1/using_rest
  */
 async function customSerach() {
-  let q = searchBar.value; 
+  let q = searchBar.value;
   let alt = "json";
   let numOfQuery = 5;
   let lang = "lang_en";
-  let url = "https://www.googleapis.com/customsearch/v1?key=" + API_KEY + "&cx=" + SearchEngineID + "&alt=" + alt + "&num=" + numOfQuery + "&q=" + q + "&lr=" + lang;
+  let url =
+    "https://www.googleapis.com/customsearch/v1?key=" +
+    API_KEY +
+    "&cx=" +
+    SearchEngineID +
+    "&alt=" +
+    alt +
+    "&num=" +
+    numOfQuery +
+    "&q=" +
+    q +
+    "&lr=" +
+    lang;
 
   let response = await fetch(url);
-  
+
   if (response.ok) {
     let data = await response.json();
     searchListDOM.innerHTML = "";
     if (data.items && data.items.length > 0) {
       for (let index = 0; index < data.items.length; index++) {
-        if(data.items[index].link.includes("en-US")) {
-          let title = data.items[index].title.replace(/</g, '&lt;').replace(/>/g, '&gt;').split("|")[0];
-          searchListDOM.innerHTML += `<li><a href="${data.items[index].link}" target="_blank">${title}</a></li>`;
+        if (data.items[index].link.includes("en-US")) {
+          let title = data.items[index].title
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .split("|")[0];
+          searchListDOM.innerHTML += `<li><a href="${
+            data.items[index].link
+          }" target="_blank">${title}</a></li>`;
         }
       }
     }
-  }else {
+  } else {
     searchListDOM.innerHTML = "";
   }
 }
