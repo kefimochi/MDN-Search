f5a8df4d189ec92c8ffe4c337796f139f97d4069
Anoop Chauhan <chauhananu0567@gmail.com>
2019-05-20 07:07:40 +0530

    search suggestion added ( Custom Google Search )

--- V.1.2-UX Friendly/content.js
+++ V.1.2-UX Friendly/content.js
@@ -1,6 +1,14 @@
 let ele = document.getElementById("form");
 let icon = document.getElementById("search-icon");
 
+// DOM Elements
+let searchBar = document.getElementById("search-bar");
+let searchListDOM = document.getElementById("serach-list");
+
+// google custom search keys
+let API_KEY = "AIzaSyAF6PrEEbmZOb1lJqVWwiAQpg3ysI0N608";
+let SearchEngineID = "005261878360177818850:u0b8m6c4ejo";
+
 if (ele.addEventListener) {
   ele.addEventListener("submit", callback, false); //Modern browsers
   icon.addEventListener("click", callback);
@@ -20,7 +28,7 @@ function callback(e) {
   if (arrayInput[1] === undefined) {
     let website = `https://developer.mozilla.org/en-US/search?q=${
       arrayInput[0]
-    }`;
+      }`;
     window.open(website, "_blank");
   } else {
     let website = "https://developer.mozilla.org/en-US/search?q=";
@@ -31,9 +39,39 @@ function callback(e) {
 }
 
 window.onload = function() {
-  document.getElementById("search-bar").focus();
+  searchBar.focus();
 };
 
-window.addEventListener("keydown", () => {
-  console.log("hi");
+// events occurs when searchbar element gets user input
+searchBar.addEventListener("input", () => {
+  customSerach();
 });
+
+/**
+ * Google Custom Search | 
+ * Resource https://developers.google.com/custom-search/v1/using_rest
+ */
+async function customSerach() {
+  let q = searchBar.value; 
+  let alt = "json";
+  let numOfQuery = 5;
+  let lang = "lang_en";
+  let url = "https://www.googleapis.com/customsearch/v1?key=" + API_KEY + "&cx=" + SearchEngineID + "&alt=" + alt + "&num=" + numOfQuery + "&q=" + q + "&lr=" + lang;
+
+  let response = await fetch(url);
+  
+  if (response.ok) {
+    let data = await response.json();
+    searchListDOM.innerHTML = "";
+    if (data.items && data.items.length > 0) {
+      for (let index = 0; index < data.items.length; index++) {
+        if(data.items[index].link.includes("en-US")) {
+          let title = data.items[index].title.replace(/</g, '&lt;').replace(/>/g, '&gt;').split("|")[0];
+          searchListDOM.innerHTML += `<li><a href="${data.items[index].link}" target="_blank">${title}</a></li>`;
+        }
+      }
+    }
+  }else {
+    searchListDOM.innerHTML = "";
+  }
+}
